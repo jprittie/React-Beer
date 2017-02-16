4499032c2dad3c82c88356624452ec2f9022bf2a
jprittie <eyewallop@gmail.com>
2017-02-16 16:18:54 -0500

    Displays details for a single beer

--- src/components/Single.js
+++ src/components/Single.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import Loader from './Loader';
 
 class Single extends React.Component {
 
+  constructor () {
+    super();
+    this.state = {
+      beer: [],
+      loading: true
+    }
+  }
+
+  componentWillMount() {
+    this.loadBeer();
+  }
+
+  loadBeer = () => {
+    const beerId = this.props.match.params.beerId
+    fetch(`http://api.react.beer/v2/beer/${beerId}`)
+    .then(data => data.json())
+    .then(result => {
+      console.log('it worked');
+      this.setState({ beer: result.data, loading: false });
+
+    })
+    .catch(err => console.error(err));
+  }
 
   render () {
+    if(this.state.loading) {
+      return <Loader message="Loading...." />
+    }
+    const { name, labels, id } = this.state.beer;
+    const image = labels.medium || 'not-found.jpg';
+
     return(
       <div className="single-beer">
-        A single beer will go here
+        <h2>{name}</h2>
+        <img src={image} alt={name} />
       </div>
     )
   }

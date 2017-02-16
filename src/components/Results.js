4c4f18e81ae239d94b14f42cafadd6a837cf099c
jprittie <eyewallop@gmail.com>
2017-02-16 16:18:36 -0500

    Shows loader

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import Beer from './Beer';
+import Loader from './Loader';
+
 
 class Results extends React.Component {
 
 
   render () {
+    if(this.props.loading) {
+      return <Loader message="Pouring a cold one!" />
+
+    }
     return(
       <div className="beers">
-        Results go here
+        {console.log(this.props.beers)}
+        {this.props.beers.map( detail => <Beer details={detail} key={detail.id}/> )}
       </div>
     )
   }

be1da5a6303545c2921627214ea6d3da0652d5d2
jprittie <eyewallop@gmail.com>
2017-02-16 16:18:05 -0500

    Renders beer name and image

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import slug from 'slugify';
+import { Link } from 'react-router-dom';
 
 class Beer extends React.Component {
 
 
   render () {
+    const { name, labels, id } = this.props.details;
+    const image = labels ? labels.medium : 'notfound.jpg';
+
     return(
       <div className="beer">
-        Beer will go here
+        <Link to={`/beer/${id}/${slug(name)}`}>
+          <h2>{name}</h2>
+          <img src={image} alt={name} />
+
+        </Link>
       </div>
     )
   }

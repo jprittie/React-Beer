06942a05c53cc341deb218d64a33f17ab00b5204
jprittie <eyewallop@gmail.com>
2017-02-16 12:37:48 -0500

    Adds routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './style.css';
 import Main from './components/Main';
+import Single from './components/Single';
 
-render(<Main />, document.querySelector('#root'));
+const Root = function() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={Main} />
+        <Route path="/search/:searchTerm" component={Main} />
+        <Route path="/beer/:beerId/:beerSlug" component={Single} />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+render(<Root />, document.querySelector('#root'));
